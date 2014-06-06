24650097324e402076045a00912bac213e9072cf
Matthew Mills <mjohnmills@gmail.com>
2014-06-05 23:29:16 -0700

    Create Interface.f90

---
+++ Interface.f90
@@ -0,0 +1,19 @@
+PROGRAM Interface
+
+IMPLICIT NONE
+
+integer :: nSteps, step
+character :: calculationArray(:)
+
+!Determine the calculation type array requested, or expand a single keyword to an array.
+do step=1,nSteps
+  !Log the start time
+  select case (trim(adjustl(CalculationArray(step))))
+  case ("PROPAGATE")
+  case default
+    !Calculation type is not recognised
+  end select
+  !Print the end time
+enddo
+
+ENDPROGRAM Interface
