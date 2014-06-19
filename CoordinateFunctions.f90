54228fc9404024b7f5d70716323735066b215e3f
MJLMills <mjohnmills@gmail.com>
2014-06-18 19:11:30 -0700

    some updates towards compilation

--- CoordinateFunctions.f90
+++ CoordinateFunctions.f90
@@ -6,72 +6,76 @@ IMPLICIT NONE
   PURE REAL(8) FUNCTION EuclideanDistance(origin,coords,n) result(r)
 
   real(8), intent(in) :: origin(n), coords(n)
-  integer :: n
+  integer, intent(in) :: n
 
     r = dsqrt(DotProduct(coords(:) - origin(:),n))
   
   END FUNCTION EuclideanDistance
-!*
-  PURE REAL(8) FUNCTION EuclideanDistanceDerivative(origin,coords,n,i) result(der)
-  
-  real(8), intent(in) :: origin(3), coords(3)
-  integer :: n, i
-  
-    der = -1.0d0 / (2.0d0 * EuclideanDistance(coord(:)-origin(:))) * 2.0d0 * coord(i)-origin(i)
-  
-  END FUNCTION EuclideanDistanceDerivative
-!*
-  PURE REAL(8) FUNCTION Angle(origin,a,b) result(theta)
-  
-  real(8), intent(in) :: a(3), b(3), origin(3)
-  real(8) :: ao(3), bo(3)
 
-    ao(:) = a(:) - o(:)
-    bo(:) = b(:) - o(:)
-    theta = dacos(DotProduct(ao, bo, 3) / (EuclideanNorm(a) * EuclideanNorm(b)))
-  
-  FUNCTION EuclideanDistance
-!*
-  PURE REAL(8) FUNCTION EuclideanNorm(vector,n) result(r)
+  PURE REAL(8) FUNCTION DotProduct(p,n) result(pdotp)
 
-  real(8), intent(in) :: vector(n)
-  integer :: n, i
-  
-    r = dsqrt(pdotq(vector,vector,n))
-  
-  END FUNCTION EuclideanNorm
-!*
-  PURE REAL(8) FUNCTION DotProduct(p,q,n), result(pdotq)
-  
-  real(8), intent(in) :: p(n), q(n)
-  integer :: n
-  
+  real(8), intent(in) :: p(n)
+  integer, intent(in) :: n
+  integer :: i
+
     do i = 1, n
-      pdotq = pdotq + ( p(i) * q(i) )
+      pdotp = pdotp + p(i) * p(i)
     enddo
-  
+
   END FUNCTION DotProduct
-!*  
-  PURE REAL(8) FUNCTION DotProduct(p,n), result pdotp
-  
-  real(8), intent(in) :: q(n)
-  integer :: n
-  
-    do i = 1, n
-      pdotp = pdotp + q(i) * q(i)
-    enddo
-  
-  END FUNCTION DotProduct
-!*  
-  PURE REAL(8) FUNCTION CrossProduct(p,q), result(pcrossq)
 
-  real(8), intent(in) :: p(3), q(3)
-  real(8), intent(out) :: pcrossq(3)
-
-    pcrossq(1) = p(2)*q(3) - p(3)*q(2)
-    pcrossq(2) = p(3)*q(1) - p(1)*q(3)
-    pcrossq(3) = p(1)*q(2) - p(2)*q(1)
-
-  END FUNCTION CrossProduct
-  
 END MODULE CoordinateFunctions
+
+!*
+!  PURE REAL(8) FUNCTION EuclideanDistanceDerivative(origin,coords,n,i) result(der)
+!  
+!  real(8), intent(in) :: origin(3), coords(3)
+!  integer :: n, i
+!  
+!    der = -1.0d0 / (2.0d0 * EuclideanDistance(coord(:)-origin(:))) * 2.0d0 * coord(i)-origin(i)
+!  
+!  END FUNCTION EuclideanDistanceDerivative
+!*
+!  PURE REAL(8) FUNCTION Angle(origin,a,b) result(theta)
+!  
+!  real(8), intent(in) :: a(3), b(3), origin(3)
+!  real(8) :: ao(3), bo(3)
+!
+!    ao(:) = a(:) - o(:)
+!    bo(:) = b(:) - o(:)
+!    theta = dacos(DotProduct(ao, bo, 3) / (EuclideanNorm(a) * EuclideanNorm(b)))
+  
+!  FUNCTION EuclideanDistance
+!*
+!  PURE REAL(8) FUNCTION EuclideanNorm(vector,n) result(r)
+
+!  real(8), intent(in) :: vector(n)
+!  integer :: n, i
+!  
+!    r = dsqrt(pdotq(vector,vector,n))
+  
+!  END FUNCTION EuclideanNorm
+!*
+!  PURE REAL(8) FUNCTION DotProduct(p,q,n), result(pdotq)
+!  
+!  real(8), intent(in) :: p(n), q(n)
+!  integer :: n
+!  
+!    do i = 1, n
+!      pdotq = pdotq + ( p(i) * q(i) )
+!    enddo
+!  
+!  END FUNCTION DotProduct
+!*  
+!  PURE REAL(8) FUNCTION CrossProduct(p,q), result(pcrossq)
+!
+!  real(8), intent(in) :: p(3), q(3)
+!  real(8), intent(out) :: pcrossq(3)
+!
+!    pcrossq(1) = p(2)*q(3) - p(3)*q(2)
+!    pcrossq(2) = p(3)*q(1) - p(1)*q(3)
+!    pcrossq(3) = p(1)*q(2) - p(2)*q(1)
+!
+!  END FUNCTION CrossProduct
+  
+!END MODULE CoordinateFunctions
--- TestBond.f90
+++ TestBond.f90
@@ -1,13 +1,16 @@
 PROGRAM TestBond
 use ForceFieldFunctions
+use CoordinateFunctions
 IMPLICIT NONE
 
-Character(LENGTH=5), allocatable :: BondTypes(:)
-integer :: nAtoms
+Character(LEN=4), allocatable :: BondTypes(:)
+Character(LEN=4) :: type_i
+integer :: nAtoms, nBonds
 integer, allocatable :: bondMatrix(:,:), BondIDs(:,:)
 integer :: i, j
 real(8), allocatable :: bondEnergies(:), bondForceConstants(:), bondReferences(:)
 real(8), allocatable :: cartesianCoordinates(:,:), atomicForces(:,:)
+real(8) :: r
 
 nAtoms = 2
 
@@ -15,6 +18,7 @@ allocate(cartesianCoordinates(nAtoms,3))
 allocate(bondMatrix(nAtoms,nAtoms))
 bondMatrix(1,2) = 1
 
+!Count the Bonds
 do i = 1, nAtoms
   do j = i+1, nAtoms
     if (bondMatrix(i,j) == 1) then
@@ -22,6 +26,7 @@ do i = 1, nAtoms
     endif
   enddo
 enddo
+!
 
 allocate(BondTypes(nBonds))
 allocate(BondIDs(nBonds,2))
@@ -35,18 +40,29 @@ bondTypes(1) = "HARM"
 bondEnergies(1) = 0.0d0
 
 do i = 1, nBonds
-  r = EuclideanDistance(cartesianCoordinates(BondIDs(i,1),:),cartesianCoordinates(BondIDs(i,2)),3)
-  select case (trim(ladjust(BondTypes(i))))
+    write(*,*) "BOND ", i, " between ", bondIDs(i,1), " and ",&
+&               bondIDs(i,2), " IS TYPE ", bondTypes(i)
+enddo
+
+do i = 1, nBonds
+  r = EuclideanDistance(cartesianCoordinates(BondIDs(i,1),:),&
+&                       cartesianCoordinates(BondIDs(i,2),:),3)
+
+  type_i = trim(adjustl(BondTypes(i)))  
+  select case (type_i)
+
   case ("HARM")
     bondEnergies(i) = HarmonicEnergy(bondForceConstants(i),)
     do j = 1, 3
-      AtomicForces(BondIDs(i,1),j) += HarmonicEnergyDerivative(j)
-      AtomicForces(BondIDs(i,2),j) -= HarmonicEnergyDerivative(j)
+      AtomicForces(BondIDs(i,1),j) = AtomicForces(BondIDs(i,1),j) + &
+&                                    HarmonicEnergyDerivative(j)
+      AtomicForces(BondIDs(i,2),j) = AtomicForces(BondIDs(i,2),j) - &
+&                                    HarmonicEnergyDerivative(j)
     enddo
-  case ("MORSE")
-    bondEnergies(i) = MorseEnergy()
+  !case ("MORSE")
+    !bondEnergies(i) = MorseEnergy()
   case default
-    !Bond Type is unknown
+    write(*,*) "Bond Type is unknown"
   end select
 enddo
 
